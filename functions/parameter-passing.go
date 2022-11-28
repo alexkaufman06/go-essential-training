a4c78d8b73f4929a20c0b58e9ec87fbadf2f1fc6
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2022-11-27 16:57:32 -0800

    Add sections on functions and parameter passing with pointer

---
+++ functions/parameter-passing.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	values := []int{1, 2, 3, 4}
+	doubleAt(values, 2)
+	fmt.Println(values)
+
+	val := 10
+	double(&val)
+	fmt.Println(val)
+}
+
+func doubleAt(values []int, i int) {
+	values[i] *= 2
+}
+
+func double(n *int) {
+	*n *= 2
+}
