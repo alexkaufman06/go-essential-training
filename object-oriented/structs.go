b70c3cb9d90aa7b6f34ed36427c522fdb894210c
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2023-01-23 19:09:31 -0800

    Methods added to struct

--- object-oriented/structs.go
+++ object-oriented/structs.go
@@ -11,15 +11,28 @@ type Budget struct {
 	Expires    time.Time
 }
 
+func (b Budget) TimeLeft() time.Duration { // (b Budget) is a receiver, it adds a method to the Budget struct
+	return b.Expires.Sub(time.Now().UTC())
+}
+
+func (b *Budget) Update(sum float64) { // Because we are changing the balance within the struct, we pass a pointer *
+	b.Balance += sum
+}
+
 func main() {
 	b1 := Budget{
 		CampaignId: "Give rich people more money",
 		Balance:    1000000,
-		Expires:    time.Now().Add(7 * 24 * time.Hour),
+		Expires:    time.Now().UTC().Add(7 * 24 * time.Hour),
 	}
 
 	fmt.Println("Print the contents of the struct:")
 	fmt.Println(b1)
+	fmt.Println("Print the time left for the above Budget:")
+	fmt.Println(b1.TimeLeft())
+	fmt.Println("Print the balance after subtracting 100000:")
+	b1.Update(-100)
+	fmt.Println(b1.Balance)
 	fmt.Println("Print contents of scruct with property information:")
 	fmt.Printf("%#v\n", b1)
 	fmt.Println("Print the CampaignId:")
