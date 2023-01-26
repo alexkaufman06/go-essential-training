b6ab36c3e9948b0c2af4d49df80dc4db0b44a95e
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2023-01-25 20:12:28 -0800

    Add interfaces challenge

---
+++ object-oriented/interfaces-challenge.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+// Capper implements io.Writer and turns everything to uppercase
+type Capper struct {
+	Writer io.Writer
+}
+
+func (c *Capper) Write(p []byte) (n int, err error) {
+	diff := byte('a' - 'A')
+	out := make([]byte, len(p))
+	for i, c := range p {
+		if c >= 'a' && c <= 'z' {
+			c -= diff
+		}
+		out[i] = c
+	}
+
+	return c.Writer.Write(out)
+}
+
+func main() {
+	c := &Capper{os.Stdout}
+	fmt.Fprintln(c, "Hello there")
+}
