2f545e81a893425c67ec5afeca0c90195930b61d
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2023-01-30 18:00:39 -0800

    Add generic example

---
+++ object-oriented/generics.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Ordered interface {
+	int | float64 | string
+}
+
+// [T Ordered] = Type T with generic type and constraint that it is Ordered
+func min[T Ordered](values []T) (T, error) {
+	if len(values) == 0 {
+		var zero T
+		return zero, fmt.Errorf("min of empty slice")
+	}
+
+	m := values[0]
+	for _, v := range values[1:] {
+		if v < m {
+			m = v
+		}
+	}
+	return m, nil
+}
+
+func main() {
+	fmt.Println(min([]float64{2, 1, 3}))
+	fmt.Println(min([]string{"B", "A", "C"}))
+}
