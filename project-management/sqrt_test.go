cf9ecd08a98ed5f12a5762eb269a8ac55e8d7074
Alex Kaufman <7607798+alexkaufman06@users.noreply.github.com>
2023-02-13 16:42:20 -0800

    Testing example

---
+++ project-management/sqrt_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func almostEqual(v1, v2 float64) bool {
+	return Abs(v1-v2) <= 0.001
+}
+
+func TestSimple(t *testing.T) {
+	val, err := Sqrt(2)
+
+	if err != nil {
+		t.Fatalf("error in calculation - %s", err)
+	}
+
+	if !almostEqual(val, 1.414214) {
+		t.Fatalf("bad value - %f", val)
+	}
+}
+
+type testCase struct {
+	value    float64
+	expected float64
+}
+
+func TestMany(t *testing.T) {
+	testCases := []testCase{
+		{0.0, 0.0},
+		{2.0, 1.414214},
+		{9.0, 3.0},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%f", tc.value), func(t *testing.T) {
+			out, err := Sqrt(tc.value)
+			if err != nil {
+				t.Fatal("error")
+			}
+
+			if !almostEqual(out, tc.expected) {
+				t.Fatalf("%f != %f", out, tc.expected)
+			}
+		})
+	}
+}
+
+// Can run this in terminal: go test
+// Name of tests: go test -v
+// If you have sub packages, this will test recursively: go test -v ./...
